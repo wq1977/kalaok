fb04fac51fee9ef84769f6c24e065c0ad33a8337
wesley wang <9610848@qq.com>
2015-12-26 14:21:18 +0800

    model base

---
+++ classes/Model.py
@@ -0,0 +1,13 @@
+class Model:
+    events={}
+
+    def initEvent(self,event):
+        Model.events[event]=[]
+
+    @staticmethod
+    def regist(self, object_method, event):
+        Model.events[event].append(object_method)
+
+    def broadcast(self,event):
+        for m in Model.events[event]:
+            m()
