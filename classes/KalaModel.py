375345c2c2b2294dfca325f063fee750e9e77bcb
wesley wang <9610848@qq.com>
2015-12-27 21:02:01 +0800

    add python script

--- classes/KalaModel.py
+++ classes/KalaModel.py
@@ -7,6 +7,12 @@ class KalaModel(Model):
         self.micReady=True
         args = shlex.split("alsaloop -C hw:1,0 -P hw:0,1 -t 1000")
         self.micProcess = subprocess.Popen(args)
+        args = shlex.split("python /home/pi/KalaOK/dlsong.py")
+        self.dsProcess = subprocess.Popen(args)
+        args = shlex.split("python /home/pi/KalaOK/playsong.py")
+        self.psProcess = subprocess.Popen(args)
+        args = shlex.split("python /home/pi/KalaOK/ctrlsong.py")
+        self.csProcess = subprocess.Popen(args)
         
     def poll1s(self,sc):
         if self.micProcess != None:
