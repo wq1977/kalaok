5ab4d6c3994330ff975bd548e165b1356b91325f
wesley wang <9610848@qq.com>
2016-01-01 16:39:47 +0800

    remove mic

--- web/ordermenu.php
+++ web/ordermenu.php
@@ -6,10 +6,10 @@
 <?php
 include_once("db.php");
 if (isset($_GET["artist"]) && strlen($_GET["artist"])>0){
-
+$value=trim($value);
 $GLOBAL_TYPE_PARAM = "d7bd83313b5b444";
 $GLOBAL_TIANLAI_KEY = "6ae993829ca5410e888f5e97b73ee4a810bb242149fd46ada5777edf587b4225";
-$request = base64_encode("{\"firstSize\":0,\"type\":2,\"common\":{\"clientversion\":\"3.3.9\",\"model\":\"sdk\",\"imei\":\"000000000000000\",\"userid\":0,\"resolution\":\"1196X720\",\"apiversion\":\"3.2.0\",\"product\":\"KALAOK\",\"clienttype\":\"Android\",\"nettype\":\"epc.tmobile.com\",\"updatechannel\":\"37\",\"login\":0,\"language\":1,\"imsi\":\"89014103211118510720\",\"systemversion\":\"17\",\"channel\":\"YYH\"},\"maxSize\":300,\"keyWord\":\"" . $_GET["artist"] . "\"}");
+$request = base64_encode("{\"firstSize\":0,\"type\":2,\"common\":{\"clientversion\":\"3.3.9\",\"model\":\"sdk\",\"imei\":\"000000000000000\",\"userid\":0,\"resolution\":\"1196X720\",\"apiversion\":\"3.2.0\",\"product\":\"KALAOK\",\"clienttype\":\"Android\",\"nettype\":\"epc.tmobile.com\",\"updatechannel\":\"37\",\"login\":0,\"language\":1,\"imsi\":\"89014103211118510720\",\"systemversion\":\"17\",\"channel\":\"YYH\"},\"maxSize\":300,\"keyWord\":\"" . $value . "\"}");
 $sign=md5($request . $GLOBAL_TIANLAI_KEY);
 $url="http://sns.audiocn.org/tlcysns/content/search.action?request=" . $request . "&sign=" . $sign ."&type=". $GLOBAL_TYPE_PARAM;
 $html = file_get_contents($url);
@@ -32,7 +32,7 @@ $html = file_get_contents($url);
 
 }
 else{
-    $sql="select * from songs,(select distinct (which) from orders order by `when` desc limit 10) as t1 where songs.id=t1.`which`";
+    $sql="select * from songs,(select distinct (which) from orders where `status`=2 order by `when` desc limit 10) as t1 where songs.id=t1.`which`";
     $ret=mysql_query($sql);
 ?>
 <ul data-role="listview" data-split-icon="gear" id="searchlist" data-split-theme="a" data-inset="true">
